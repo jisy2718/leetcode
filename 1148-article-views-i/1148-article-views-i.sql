85bf23726edbbf2b11786ef061d1c0d498343e4a
jisy2718 <97325455+jisy2718@users.noreply.github.com>
2022-12-12 10:04:33 +0900

    Time: 482 ms (85.27%), Space: 0B (100.00%) - LeetHub

---
+++ 1148-article-views-i/1148-article-views-i.sql
@@ -0,0 +1,81 @@
+select distinct author_id id from Views
+where author_id = viewer_id
+order by id
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Write your MySQL query statement belows
+# select distinct author_id id from views
+# where author_id = viewer_id
+# order by id
+
+
+
+# select distinct author_id id from views
+# where author_id = viewer_id
+# order by id
+
+
+
+# select author_id id from views
+# where author_id = viewer_id
+# group by author_id
+# order by id
+
+
+
