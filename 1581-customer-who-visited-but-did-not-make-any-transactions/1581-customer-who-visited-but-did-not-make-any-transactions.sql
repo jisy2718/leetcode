269e57563d2fb413711b3254c27d4c5e88ff0227
jisy2718 <97325455+jisy2718@users.noreply.github.com>
2022-12-12 09:49:46 +0900

    Time: 1655 ms (39.52%), Space: 0B (100.00%) - LeetHub

---
+++ 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -0,0 +1,94 @@
+# visit과 transaction의 jointable을 만든 후에, transaction이 없는 row들만 세어주기
+select customer_id, count(*) count_no_trans
+from
+(select * from Visits v
+left join Transactions t
+using(visit_id)
+) visit_transaction_join
+
+where transaction_id is null
+group by customer_id
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Write your MySQL query statement below
+
+# SELECT customer_id, count(customer_id) count_no_trans
+# from visits v
+# left join transactions t
+# on v.visit_id = t.visit_id
+# where t.transaction_id is null
+# group by customer_id
+
+# # t의 visit_id에 없는 v의 visit_id에 대응하는 customer_id와 그 개수 찾기
+# select customer_id, count(customer_id) count_no_trans from Visits v
+# left join Transactions t
+# on v.visit_id = t.visit_id
+# where transaction_id IS NULL
+# group by customer_id
+
