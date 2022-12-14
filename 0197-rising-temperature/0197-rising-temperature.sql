1a0b5266d1e63363c5342afd3bfcfdddfe3f3597
jisy2718 <97325455+jisy2718@users.noreply.github.com>
2022-12-14 15:50:36 +0900

    Time: 1298 ms (9.60%), Space: 0B (100.00%) - LeetHub

---
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -0,0 +1,67 @@
+# table 2개 가져와서, 비교하기
+select w1.id from Weather w1, Weather w2
+where datediff( w1.recordDate,  w2.recordDate) = 1   # datediff(날짜1,날짜2) = 날짜1 - 날짜2 의 일수
+and w1.temperature > w2.temperature
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # Write your MySQL query statement belo
+
+# select w2.Id id
+# from weather w1, weather w2
+# where subdate(w2.recordDate, 1) = w1.recordDate AND w1.temperature < w2.temperature
