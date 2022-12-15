788848411ea77eb86cbccb2c1214d5bff054b7de
jisy2718 <97325455+jisy2718@users.noreply.github.com>
2022-12-15 13:40:29 +0900

    Time: 1497 ms (5.03%), Space: 0B (100.00%) - LeetHub

---
+++ 1729-find-followers-count/1729-find-followers-count.sql
@@ -0,0 +1,65 @@
+select user_id, count(distinct follower_id) followers_count from Followers
+group by user_id
+order by user_id
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Write your MySQL query statement below
+# select user_id, count(*) followers_count from followers
+# group by user_id
+# order by user_id
